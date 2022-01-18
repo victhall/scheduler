63d4c5bae3852e7e8b0ac4ded213c15eb912017a
victhall <victoriahall.dev@gmail.com>
2022-01-18 17:39:09 -0500

    build and refactored button, built dayListItem

---
+++ src/components/DayListItem.js
@@ -0,0 +1,16 @@
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  let itemClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots <= 0,
+  });
+ 
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
